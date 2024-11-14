2eb5758ba3a4a0164f7cd08dea551323c5ada928
Ismail Bourbie <bourbieismail@gmail.com>
2024-11-14 12:07:17 +0100

    Clean up tests by removing duplicated assertions

--- resources/views/medicines/index.blade.php
+++ resources/views/medicines/index.blade.php
@@ -9,13 +9,29 @@
 </head>
 <body>
 
-<ul>
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Trade Name</th>
+        <th>Dci</th>
+        <th>Dosage</th>
+        <th>Form</th>
+        <th>Packaging</th>
+    </tr>
+    </thead>
+    <tbody>
     @foreach($medicines as $medicine)
-        <li>
-            {{$medicine->name}} ({{$medicine->dci->pluck('name')->implode('/')}}
-            ) {{$medicine->dci->pluck('details.dosage')->implode('/')}} {{$medicine->dci->first()->details->form}} {{$medicine->dci->first()->details->packaging}}
-        </li>
+        <tr>
+            <td>{{$loop->index}}</td>
+            <td>{{strtoupper($medicine->name)}}</td>
+            <td>{{$medicine->dci->pluck('name')->map(function ($string) {return ucwords($string);})->implode('/')}}</td>
+            <td>{{$medicine->dci->pluck('details.dosage')->implode('/')}}</td>
+            <td>{{$medicine->dci->first()->details->form}}</td>
+            <td>{{strtoupper($medicine->dci->first()->details->packaging)}}</td>
+        </tr>
     @endforeach
-</ul>
+    </tbody>
+</table>
 </body>
 </html>
--- tests/Medicines/Controllers/MedicinesControllerTest.php
+++ tests/Medicines/Controllers/MedicinesControllerTest.php
@@ -25,7 +25,6 @@ class MedicinesControllerTest extends TestCase
     #[Test]
     public function it_show_all_existed_medicines_names(): void
     {
-        $this->withoutExceptionHandling();
         $medicines = MedicineFactory::new()->count(5)->create()->each(function (Medicine $medicine) {
             $medicine->dci()->attach(
                 DciFactory::new()->createOne(),
@@ -38,74 +37,15 @@ class MedicinesControllerTest extends TestCase
         });
         $response = $this->get(route('medicines.index'));
 
-        $response->assertSee($medicines->pluck('name')->toArray());
+        $response->assertSee(
+            $medicines->pluck('name')
+                ->map(fn($str) => strtoupper($str))
+                ->toArray()
+        );
     }
 
     #[Test]
-    public function it_show_medicines_with_their_dci(): void
-    {
-        $this->withoutExceptionHandling();
-        $dci = DciFactory::new()->createOne();
-        $medicine = MedicineFactory::new()->createOne();
-
-        $medicine->dci()->attach($dci, [
-            'form' => 'COMP',
-            'dosage' => '1000mg',
-            'packaging' => 'bte 8',
-        ]);
-
-        $response = $this->get(route('medicines.index'));
-
-        $response->assertSee($medicine->name);
-        $response->assertSee($medicine->dci->first()->name);
-    }
-
-    #[Test]
-    public function it_show_medicines_with_form_and_dose_and_packaging(): void
-    {
-        $dci = DciFactory::new()->createOne(['name' => 'paracetamol']);
-        $medicine = MedicineFactory::new()->createOne(['name' => 'Doliprane']);
-
-        $medicine->dci()->attach($dci, [
-            'form' => 'COMP',
-            'dosage' => '1000mg',
-            'packaging' => '8',
-        ]);
-
-        $response = $this->get(route('medicines.index'));
-
-        $response->assertSee($medicine->dci->first()->details->form);
-        $response->assertSee($medicine->dci->first()->details->dosage);
-        $response->assertSee($medicine->dci->first()->details->packaging);
-    }
-
-    #[Test]
-    public function it_show_medicine_with_combined_dci(): void
-    {
-        $amlodipine = DciFactory::new()->createOne(['name' => 'amlodipine']);
-        $valsartan = DciFactory::new()->createOne(['name' => 'valsartan']);
-        $medicine = MedicineFactory::new()->createOne(['name' => 'exval']);
-
-        $medicine->dci()->attach($amlodipine, [
-            'form' => 'COMP',
-            'dosage' => '5mg',
-            'packaging' => 'Bte 30',
-        ]);
-        $medicine->dci()->attach($valsartan, [
-            'form' => 'COMP',
-            'dosage' => '80mg',
-            'packaging' => 'Bte 30',
-        ]);
-
-        $response = $this->get(route('medicines.index'));
-
-        $response->assertSee($medicine->name);
-        $response->assertSee($medicine->dci->first()->name);
-        $response->assertSee($medicine->dci->last()->name);
-    }
-
-    #[Test]
-    public function it_show_medicine_on_formatted_way(): void
+    public function it_show_medicine_on_with_full_details(): void
     {
         $amlodipine = DciFactory::new()->createOne(['name' => 'amlodipine']);
         $valsartan = DciFactory::new()->createOne(['name' => 'valsartan']);
@@ -113,6 +53,7 @@ class MedicinesControllerTest extends TestCase
         $exval = MedicineFactory::new()->createOne(['name' => 'exval']);
         $amlor = MedicineFactory::new()->createOne(['name' => 'amlor']);
 
+        // With Combined DCI
         $exval->dci()->attach($amlodipine, [
             'form' => 'COMP',
             'dosage' => '5mg',
@@ -123,7 +64,8 @@ class MedicinesControllerTest extends TestCase
             'dosage' => '80mg',
             'packaging' => 'Bte 30',
         ]);
-        $amlor->dci()->attach($valsartan, [
+        // With Single Dci
+        $amlor->dci()->attach($amlodipine, [
             'form' => 'COMP',
             'dosage' => '5mg',
             'packaging' => 'Bte 30',
@@ -131,7 +73,11 @@ class MedicinesControllerTest extends TestCase
 
         $response = $this->get(route('medicines.index'));
 
-//        $response->assertSee('exval (amlodipine/valsartan) 5mg/80mg COMP Bte 30');
-//        $response->assertSee('amlor (amlodipine) 5mg COMP Bte 30');
+        $response->assertSeeTextInOrder([
+            'EXVAL', 'Amlodipine/Valsartan', '5mg/80mg', 'COMP', 'BTE 30',
+        ]);
+        $response->assertSeeTextInOrder([
+            'AMLOR', 'Amlodipine', '5mg', 'COMP', 'BTE 30',
+        ]);
     }
 }
