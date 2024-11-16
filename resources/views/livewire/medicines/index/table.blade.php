c28ce04996dc40914622f913041af63d25160745
Ismail Bourbie <bourbieismail@gmail.com>
2024-11-16 14:34:35 +0100

    design medicine dataTable

--- resources/views/livewire/medicines/index/table.blade.php
+++ resources/views/livewire/medicines/index/table.blade.php
@@ -1,27 +1,44 @@
 <div>
-    <table>
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Trade Name</th>
-            <th>Dci</th>
-            <th>Dosage</th>
-            <th>Form</th>
-            <th>Packaging</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($medicines as $medicine)
-            <tr>
-                <td>{{$loop->index}}</td>
-                <td>{{strtoupper($medicine->name)}}</td>
-                <td>{{$medicine->dci->pluck('name')->map(function ($string) {return ucwords($string);})->implode('/')}}</td>
-                <td>{{$medicine->dci->pluck('details.dosage')->implode('/')}}</td>
-                <td>{{$medicine->dci->first()->details->form}}</td>
-                <td>{{strtoupper($medicine->dci->first()->details->packaging)}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    {{ $medicines->links() }}
+    <div class="my-12 space-y-6">
+        <h2 class="text-3xl font-medium">All Medicines</h2>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        #
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Trade Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Dci
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Dosage
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Form
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Packaging
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($medicines as $medicine)
+                    <tr class="bg-white border-b">
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{$loop->index}}</td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{strtoupper($medicine->name)}}</td>
+                        <td class="px-6 py-4">{{$medicine->dci->pluck('name')->map(function ($string) {return ucwords($string);})->implode('/')}}</td>
+                        <td class="px-6 py-4">{{$medicine->dci->pluck('details.dosage')->implode('/')}}</td>
+                        <td class="px-6 py-4">{{$medicine->dci->first()->details->form}}</td>
+                        <td class="px-6 py-4">{{strtoupper($medicine->dci->first()->details->packaging)}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            {{ $medicines->links() }}
+        </div>
+    </div>
 </div>
--- resources/views/medicines/index.blade.php
+++ resources/views/medicines/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Document</title>
-</head>
-<body>
-<livewire:medicines.index.table/>
-</body>
-</html>
+@component('layouts.base', ['title' => 'All Medicines'])
+    <div class="flex justify-center items-center">
+
+        <livewire:medicines.index.table/>
+    </div>
+@endcomponent
