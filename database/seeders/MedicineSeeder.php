fdc68ef8758cfd795412fa3327dec99a6c03dfc0
Ismail Bourbie <bourbieismail@gmail.com>
2024-12-23 23:41:49 +0100

    fixed data in Medicine Seeder, deleted unused files

--- database/seeders/MedicineSeeder.php
+++ database/seeders/MedicineSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Database\Factories\CodeFactory;
 use Database\Factories\LaboratoryFactory;
 use Database\Factories\MedicineFactory;
 use Illuminate\Database\Seeder;
@@ -11,36 +10,35 @@ class MedicineSeeder extends Seeder
 {
     public function run(): void
     {
-
-        // Some real data
-        $paracetamol = CodeFactory::new()->createOne(['name' => 'paracetamol']);
-        $codeine = CodeFactory::new()->createOne(['name' => 'codeine']);
-
         $sanofi = LaboratoryFactory::new()->createOne(['name' => 'Sanofi', 'country' => 'France']);
         $merenal = LaboratoryFactory::new()->createOne(['name' => 'Merenal', 'country' => 'Algeria']);
 
-        $doliprane = MedicineFactory::new()->withDci($paracetamol, '500mg')->for($sanofi)->createOne([
+        $doliprane = MedicineFactory::new()->for($sanofi)->createOne([
             'name' => 'doliprane',
+            'dci' => 'paracetamol',
+            'dosage' => '500mg',
             'slug' => 'doliprane-500mg-comp',
             'form' => 'COMP',
             'packaging' => 'bte 8',
         ]);
-        $dolyc = MedicineFactory::new()->withDci($paracetamol, '500mg')->for($merenal)->createOne([
+        $dolyc = MedicineFactory::new()->for($merenal)->createOne([
             'name' => 'dolyc',
+            'dci' => 'paracetamol',
+            'dosage' => '500mg',
             'slug' => 'dolyc-500mg-comp',
             'form' => 'COMP',
             'packaging' => 'bte 10',
         ]);
-        $codolipran = MedicineFactory::new()->withDci($paracetamol, '400mg')
-            ->for($sanofi)->withDci($codeine, '20mg')
-            ->createOne([
-                'name' => 'codolipran',
-                'slug' => 'codoliprane-500mg-comp',
-                'form' => 'COMP',
-                'packaging' => 'bte 10',
-            ]);
+        $codolipran = MedicineFactory::new()->for($sanofi)->createOne([
+            'name' => 'co-doliprane',
+            'dci' => 'paracetamol/codeine',
+            'dosage' => '400mg/20mg',
+            'slug' => 'codoliprane-500mg-comp',
+            'form' => 'COMP',
+            'packaging' => 'bte 20',
+        ]);
 
         // Fake Data
-        MedicineFactory::new()->count(25)->withDci()->create();
+        MedicineFactory::new()->count(25)->create();
     }
 }
