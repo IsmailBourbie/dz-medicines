03e83654ec04593cd6fbc8676fc9d68a198beb25
Ismail Bourbie <bourbieismail@gmail.com>
2024-12-29 15:17:36 +0100

    added medicines relation to Speciality through Code

--- src/Domains/Medicines/Models/Speciality.php
+++ src/Domains/Medicines/Models/Speciality.php
@@ -3,7 +3,16 @@
 namespace Domains\Medicines\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Speciality extends Model
 {
+
+    public function medicines(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Medicine::class,
+            Code::class,
+        );
+    }
 }
---
+++ tests/Medicines/Models/SpecialityTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Medicines\Models;
+
+use Database\Factories\CodeFactory;
+use Database\Factories\MedicineFactory;
+use Database\Factories\SpecialityFactory;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class SpecialityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_has_a_many_medicines_through_code(): void
+    {
+        $speciality = SpecialityFactory::new()->createOne();
+        $code = CodeFactory::new()->for($speciality)->createOne();
+        $medicine = MedicineFactory::new()->createOne(['code_id' => $code]);
+        MedicineFactory::new()->count(3)->create(['code_id' => $code]);
+
+        $this->assertNotNull($speciality->medicines);
+        $this->assertCount(4, $speciality->medicines);
+        $this->assertInstanceOf(Collection::class, $speciality->medicines);
+        $this->assertTrue($speciality->medicines->first()->is($medicine));
+    }
+
+}
