04c36631384d3f516c585c32a78ce1afd025078f
Ismail Bourbie <bourbieismail@gmail.com>
2025-01-02 14:54:07 +0100

    show laboratory medicines on medicine page

--- resources/views/laboratories/show.blade.php
+++ resources/views/laboratories/show.blade.php
@@ -1,4 +1,48 @@
 @component('layouts.base', ['title' => $laboratory->name])
     <h2>{{$laboratory->name}}</h2>
     <h5>{{$laboratory->country}}</h5>
+
+    <div class="relative  shadow-md sm:rounded-lg">
+        <div class="overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                <thead class="text-sm text-sky-800 font-bold uppercase bg-indigo-100 font-quicksand">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        #
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Trade Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Dci
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Dosage
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Form
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Packaging
+                    </th>
+                </tr>
+                </thead>
+                <tbody x-data>
+                @foreach($laboratory->medicines as $medicine)
+                    <tr
+                        class="bg-white border-b hover:cursor-pointer hover:bg-indigo-50 uppercase"
+                        wire:key="{{$medicine->id}}"
+                        x-on:click="location.href = '{{route('medicines.show', $medicine->slug)}}'">
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ ($medicines->currentPage() - 1) * $medicines->perPage() + $loop->iteration }}</td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{$medicine->name}}</td>
+                        <td class="px-6 py-4">{{$medicine->dci}}</td>
+                        <td class="px-6 py-4">{{$medicine->dosage}}</td>
+                        <td class="px-6 py-4">{{$medicine->form}}</td>
+                        <td class="px-6 py-4">{{$medicine->packaging}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endcomponent
--- tests/Laboratory/Controllers/LaboratoryControllerTest.php
+++ tests/Laboratory/Controllers/LaboratoryControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Laboratory\Controllers;
 
 use Database\Factories\LaboratoryFactory;
+use Database\Factories\MedicineFactory;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -19,6 +20,22 @@ class LaboratoryControllerTest extends TestCase
         $response->assertViewIs('laboratories.show');
         $response->assertViewHas('laboratory');
         $response->assertSeeText([$laboratory->name, $laboratory->country]);
+
+    }
+
+    #[Test]
+    public function it_show_medicines_of_laboratory(): void
+    {
+        $laboratory = LaboratoryFactory::new()->createOne();
+        $medicine = MedicineFactory::new()->for($laboratory)->createOne();
+
+        $response = $this->get(route('laboratories.show', $laboratory->id));
+
+        $response->assertSeeText([
+            $medicine->name, $medicine->dci,
+            $medicine->dosage, $medicine->form,
+            $medicine->packaging,
+        ]);
 
     }
 }
