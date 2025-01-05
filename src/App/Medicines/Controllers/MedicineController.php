3ac03dd7d000aacd81c410b207d54e122efa7237
Ismail Bourbie <bourbieismail@gmail.com>
2025-01-05 13:55:52 +0100

    fixed duplication on medicine index page

--- src/App/Medicines/Controllers/MedicineController.php
+++ src/App/Medicines/Controllers/MedicineController.php
@@ -10,7 +10,7 @@ class MedicineController extends Controller
 {
     public function index(): View
     {
-        return view('medicines.index', ['medicines' => Medicine::query()->paginate()]);
+        return view('medicines.index');
     }
 
     public function show(Medicine $medicine): View
