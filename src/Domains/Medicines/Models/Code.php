52fe283ba4e6294b848c1aa69a4ac87c15f3e5bf
Ismail Bourbie <bourbieismail@gmail.com>
2025-01-07 14:09:33 +0100

    removed unnecessary medicines relation in Code model

--- src/Domains/Medicines/Models/Code.php
+++ src/Domains/Medicines/Models/Code.php
@@ -4,7 +4,6 @@ namespace Domains\Medicines\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Code extends Model
 {
@@ -12,10 +11,5 @@ class Code extends Model
     public function class(): BelongsTo
     {
         return $this->belongsTo(MedicineClass::class);
-    }
-
-    public function medicines(): HasMany
-    {
-        return $this->hasMany(Medicine::class);
     }
 }
--- tests/Medicines/Intergration/CodeRelationshipTest.php
+++ tests/Medicines/Intergration/CodeRelationshipTest.php
@@ -4,9 +4,7 @@ namespace Tests\Medicines\Intergration;
 
 use Database\Factories\CodeFactory;
 use Database\Factories\MedicineClassFactory;
-use Database\Factories\MedicineFactory;
 use Domains\Medicines\Models\MedicineClass;
-use Illuminate\Database\Eloquent\Collection;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -22,18 +20,5 @@ class CodeRelationshipTest extends TestCase
         $this->assertNotNull($code->class);
         $this->assertInstanceOf(MedicineClass::class, $code->class);
         $this->assertTrue($code->class->is($class));
-    }
-
-    #[Test]
-    public function it_has_many_medicines(): void
-    {
-        $code = CodeFactory::new()->createOne();
-
-        $medicines = MedicineFactory::new()->count(2)->create(['code_id' => $code]);
-
-        $this->assertCount(2, $code->medicines);
-        $this->assertInstanceOf(Collection::class, $code->medicines);
-        $this->assertTrue($code->medicines->first()->is($medicines->first()));
-
     }
 }
