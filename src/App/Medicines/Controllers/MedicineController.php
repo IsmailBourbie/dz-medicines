f8bd8c45e0cf395b3fcea7ff9d34e844100ca9bd
Ismail Bourbie <bourbieismail@gmail.com>
2025-01-11 14:57:28 +0100

    use viewModel on Medicine controller show action

--- src/App/Medicines/Controllers/MedicineController.php
+++ src/App/Medicines/Controllers/MedicineController.php
@@ -3,6 +3,7 @@
 namespace App\Medicines\Controllers;
 
 use App\Controller;
+use App\Medicines\ViewModels\MedicineShowViewModel;
 use Domains\Medicines\Models\Medicine;
 use Illuminate\View\View;
 
@@ -15,16 +16,6 @@ class MedicineController extends Controller
 
     public function show(Medicine $medicine): View
     {
-        $medicine->loadMissing(['code', 'class', 'laboratory']);
-        $generics = $medicine->code->medicines()->filterOutMedicine($medicine->id)->get();
-        $classMedicines = $medicine->class->medicines()->filterOutMedicine($medicine->id)->get();
-        $labMedicines = $medicine->laboratory->medicines()->filterOutMedicine($medicine->id)->get();
-
-        return view('medicines.show', compact(
-            'medicine',
-            'generics',
-            'classMedicines',
-            'labMedicines'
-        ));
+        return view('medicines.show', new MedicineShowViewModel($medicine));
     }
 }
