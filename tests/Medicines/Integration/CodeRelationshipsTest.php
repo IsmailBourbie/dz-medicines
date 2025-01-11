1a15d06fc9f20d95f638efda933d0ee7581f9145
Ismail Bourbie <bourbieismail@gmail.com>
2025-01-11 16:02:40 +0100

    removed unnecessary medicine relation in Code model

--- tests/Medicines/Integration/CodeRelationshipsTest.php
+++ tests/Medicines/Integration/CodeRelationshipsTest.php
@@ -4,9 +4,7 @@ namespace Tests\Medicines\Integration;
 
 use Database\Factories\CodeFactory;
 use Database\Factories\MedicineClassFactory;
-use Database\Factories\MedicineFactory;
 use Domains\Medicines\Models\MedicineClass;
-use Illuminate\Database\Eloquent\Collection;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -24,15 +22,4 @@ class CodeRelationshipsTest extends TestCase
         $this->assertTrue($code->class->is($class));
     }
 
-    #[Test]
-    public function it_has_many_medicines(): void
-    {
-        $code = CodeFactory::new()->createOne();
-        $medicines = MedicineFactory::new()->count(2)->for($code)->create();
-
-        $this->assertInstanceOf(Collection::class, $code->medicines);
-        $this->assertCount(2, $code->medicines);
-        $this->assertTrue($medicines[0]->code->is($code));
-        $this->assertTrue($medicines[1]->code->is($code));
-    }
 }
