1cf0b3d2bb63ff5e0f56aeec5ea8bd9e1cbfb6c2
Ismail Bourbie <bourbieismail@gmail.com>
2025-01-20 11:26:25 +0100

    updated medicine table design

--- resources/views/livewire/medicines/index/table.blade.php
+++ resources/views/livewire/medicines/index/table.blade.php
@@ -15,15 +15,15 @@
             <tbody x-data>
             @foreach($medicines as $medicine)
                 <tr
-                    class="bg-teal-50 uppercase text-teal-900 hover:cursor-pointer hover:bg-teal-100/40"
+                    class="bg-teal-100/70 uppercase text-teal-900 hover:cursor-pointer hover:bg-teal-100"
                     wire:key="{{$medicine->id}}"
                     x-on:click="location.href = '{{route('medicines.show', $medicine->slug)}}'">
-                    <td class="rounded-s-2xl px-6 py-6 font-medium text-teal-950 whitespace-nowrap">{{ ($medicines->currentPage() - 1) * $medicines->perPage() + $loop->iteration }}</td>
+                    <td class="rounded-s-lg px-6 py-6 font-medium text-teal-950 whitespace-nowrap">{{ ($medicines->currentPage() - 1) * $medicines->perPage() + $loop->iteration }}</td>
                     <td class="px-6 py-6 font-medium text-teal-950 whitespace-nowrap">{{$medicine->name}}</td>
                     <td class="px-6 py-6">{{$medicine->dci}}</td>
                     <td class="px-6 py-6">{{$medicine->dosage}}</td>
                     <td class="px-6 py-6">{{$medicine->form}}</td>
-                    <td class="px-6 py-6 rounded-e-2xl">{{$medicine->packaging}}</td>
+                    <td class="px-6 py-6 rounded-e-lg">{{$medicine->packaging}}</td>
                 </tr>
             @endforeach
             </tbody>
