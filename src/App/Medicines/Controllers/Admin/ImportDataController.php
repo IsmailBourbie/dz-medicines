0f6881d7fd1f7fc5b87c4260772eb0b81f0c804c
Ismail Bourbie <bourbieismail@gmail.com>
2025-02-17 16:21:02 +0100

    wp: implementing the import process

--- src/App/Medicines/Controllers/Admin/ImportDataController.php
+++ src/App/Medicines/Controllers/Admin/ImportDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Medicines\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ImportDataController
@@ -9,5 +10,10 @@ class ImportDataController
     public function create(): View
     {
         return view('admin.import-data');
+    }
+
+    public function store(Request $request)
+    {
+        // process file here
     }
 }
--- tests/Medicines/Feature/DataImportControllerTest.php
+++ tests/Medicines/Feature/DataImportControllerTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Medicines\Feature;
 
+use Domains\Medicines\Models\Code;
+use Domains\Medicines\Models\Laboratory;
+use Domains\Medicines\Models\Medicine;
+use Illuminate\Http\UploadedFile;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -13,5 +17,27 @@ class DataImportControllerTest extends TestCase
         $this->get(route('admin.import-data.create'))
             ->assertSuccessful()
             ->assertViewIs('admin.import-data');
+    }
+
+    #[Test]
+    public function it_import_data_and_save_it_to_database(): void
+    {
+        $this->withoutExceptionHandling();
+        $filePath = base_path('tests\Fixtures\medicines.xlsx');
+        $file = new UploadedFile(
+            $filePath,
+            'medicines.xlsx',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            null,
+            true);
+
+        $response = $this->post(route('admin.import-data.store'), [
+            'file' => $file,
+        ]);
+
+        $response->assertSuccessful();
+        $this->assertDatabaseCount(Medicine::class, 10);
+        $this->assertDatabaseCount(Code::class, 8);
+        $this->assertDatabaseCount(Laboratory::class, 18);
     }
 }
