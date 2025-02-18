8521097055060ff399469306f95439ee24d40eeb
Ismail Bourbie <bourbieismail@gmail.com>
2025-02-18 15:34:46 +0100

    Cast Code Value in the Code Model

---
+++ src/Domains/Medicines/Casts/CodeValue.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Domains\Medicines\Casts;
+
+use Domains\Medicines\ValueObjects\CodeValue as CodeValueObject;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class CodeValue implements CastsAttributes
+{
+
+    public function get(Model $model, string $key, mixed $value, array $attributes): CodeValueObject
+    {
+        return new CodeValueObject(
+            $attributes['value'],
+        );
+    }
+
+    public function set(Model $model, string $key, $value, array $attributes): array
+    {
+        return [
+            'value' => $value,
+        ];
+    }
+}
--- src/Domains/Medicines/Models/Code.php
+++ src/Domains/Medicines/Models/Code.php
@@ -3,11 +3,20 @@ declare(strict_types=1);
 
 namespace Domains\Medicines\Models;
 
+use Domains\Medicines\Casts\CodeValue;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Code extends Model
 {
+
+    protected function casts(): array
+    {
+        return [
+            'value' => CodeValue::class,
+        ];
+    }
+
     public function class(): BelongsTo
     {
         return $this->belongsTo(MedicineClass::class);
