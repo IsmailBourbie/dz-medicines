688fdd8b690a41452bce86d5afda3595b563230a
Ismail Bourbie <bourbieismail@gmail.com>
2025-02-21 20:05:24 +0100

    imported all data using extracted classes

--- src/Domains/Medicines/Services/ImportDataService.php
+++ src/Domains/Medicines/Services/ImportDataService.php
@@ -3,17 +3,14 @@
 namespace Domains\Medicines\Services;
 
 use Domains\Medicines\DTOs\MedicineData;
-use Domains\Medicines\Models\Code;
-use Domains\Medicines\Models\Laboratory;
-use Domains\Medicines\ValueObjects\CodeValue;
 use Illuminate\Support\LazyCollection;
-use Illuminate\Support\Str;
 
 final readonly class ImportDataService implements ImportDataInterface
 {
 
-    public function __construct(private MedicineImporter $medicineImporter)
-    {
+    public function __construct(
+        private MedicineImporter $medicineImporter,
+    ) {
     }
 
     public function importAllData(LazyCollection $data): void
@@ -22,46 +19,8 @@ final readonly class ImportDataService implements ImportDataInterface
             if (!$row["CODE"]) {
                 return;
             }
-
-            $row['LABORATORY_ID'] = $this->importLaboratoryData(
-                $row["LABORATOIRES DETENTEUR DE LA DECISION D'ENREGISTREMENT"],
-                $row["PAYS DU LABORATOIRE DETENTEUR DE LA DECISION D'ENREGISTREMENT"]
-            );
-            $row['CODE_ID'] = $this->importCodeData(new CodeValue($row["CODE"]));
-
-            $medicineDTO = MedicineData::fromArray($row);
-
-            $this->medicineImporter->import($medicineDTO);
-//            Medicine::query()->create([
-//                'name' => $row["NOM DE MARQUE"],
-//                'dci' => $row["DENOMINATION COMMUNE INTERNATIONALE"],
-//                'form' => $row["FORME"],
-//                'dosage' => $row["DOSAGE"],
-//                'packaging' => $row["CONDITIONNEMENT"] ?? $row["COND"],
-//                'is_generic' => $row["TYPE"] === "GE",
-//                'is_local' => $row["STATUT"] === "F",
-//                'laboratory_id' => $this->importLaboratoryData(
-//                    $row["LABORATOIRES DETENTEUR DE LA DECISION D'ENREGISTREMENT"],
-//                    $row["PAYS DU LABORATOIRE DETENTEUR DE LA DECISION D'ENREGISTREMENT"]
-//                ),
-//                'code_id' => $this->importCodeData(new CodeValue($row["CODE"])),
-//            ]);
+            $this->medicineImporter->import(MedicineData::fromArray($row));
         });
     }
 
-
-    protected function importLaboratoryData(string $name, string $country): int
-    {
-        return Laboratory::firstOrCreate(['slug' => Str::slug($name.' '.$country)], [
-            'name' => $name,
-            'country' => $country,
-        ])->id;
-    }
-
-    protected function importCodeData(CodeValue $value): int
-    {
-        return Code::firstOrCreate(['value' => $value,], [
-            'class_id' => $value->classId(),
-        ])->id;
-    }
 }
--- src/Domains/Medicines/Services/MedicineImporter.php
+++ src/Domains/Medicines/Services/MedicineImporter.php
@@ -2,7 +2,38 @@
 
 namespace Domains\Medicines\Services;
 
+use Domains\Medicines\DTOs\MedicineData;
+use Domains\Medicines\Models\Medicine;
+use Domains\Medicines\ValueObjects\CodeValue;
+
 class MedicineImporter
 {
+    public function __construct(
+        private LaboratoryImporter $laboratoryImporter,
+        private CodeImporter $codeImporter
+    ) {
+    }
+
+    public function import(MedicineData $data): Medicine
+    {
+        $laboratory = $this->laboratoryImporter->import(
+            $data->laboratory_name,
+            $data->country
+        );
+
+        $code = $this->codeImporter->import(new CodeValue($data->code));
+
+        return Medicine::query()->create([
+            'name' => $data->name,
+            'dci' => $data->dci,
+            'form' => $data->form,
+            'dosage' => $data->dosage,
+            'packaging' => $data->packaging,
+            'is_generic' => $data->is_generic,
+            'is_local' => $data->is_local,
+            'laboratory_id' => $laboratory->id,
+            'code_id' => $code->id,
+        ]);
+    }
 
 }
